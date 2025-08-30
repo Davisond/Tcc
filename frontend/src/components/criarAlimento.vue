616d148abe2230d5e4f203e4dafb12938996f584
Davison <davisonazevedo05@gmail.com>
2025-08-30 15:35:53 -0300

    botoes enviar e fechar de criarAlimento

--- frontend/src/components/criarAlimento.vue
+++ frontend/src/components/criarAlimento.vue
@@ -1,14 +1,15 @@
     <template>
         <div class="card">
             <form @submit.prevent="cadastrarNovoAlimento">
+                <h2>Cadastre seu alimento</h2>
                 <input type="text" v-model="nome" placeholder="nome do alimento" required/>
                 <input type="Number" v-model="carboidrato" placeholder="carboidrato" required/>
                 <input type="Number" v-model="proteina" placeholder="proteina" required/>
                 <input type="Number" v-model="gordura" placeholder="gordura" required/>
                 <input type="Number" v-model="caloria" placeholder="Kcal" required/>
                 <input type="Number" v-model="porcao" placeholder="Porção em gramas de uma unidade" required/>
-            <button type="submit">Cadastrar alimento personalizado</button>
-            <button type="button" @click="$emit('fechar')">Cancelar</button>
+            <button type="submit" class="enviar">Cadastrar!</button>
+            <button type="button" class="fechar" @click="$emit('fechar')">x</button>
         </form>
         </div>
     </template>
@@ -69,3 +70,60 @@
             }
         }
     </script>
+
+    <style scoped>
+        .card {
+            max-width: 300px;
+            margin: 2rem auto;
+            padding: 2rem;
+            background-color: #f3e8fd; 
+            border-radius: 12px;
+            box-shadow: 0 5px 5px rgba(0, 0, 0, 0.1);
+            font-family: 'Arial', sans-serif;
+        }
+        input[type= "text"],[type= "Number"]{
+            background-color: e0c7fa;
+            padding: 0.7rem 1.7rem;
+            margin-bottom: 1.0rem;
+            border-radius: 8px;
+        }
+        h2{
+            text-align: center;
+            color: #2c3e50;
+            margin-bottom: 1.7rem;
+            font-weight: bold;
+        }
+        .enviar{
+            font-size: 1rem;
+            background-color: #00ff158e;
+            border-top-left-radius: 8px;
+            border-bottom-left-radius: 8px;
+            box-shadow: 0 5px 5px rgba(0, 0, 0, 0.1);
+            margin: 1rem auto;
+        }
+        .enviar:hover{
+            font-size: 1rem;
+            background-color: #00a70e;
+            border-top-left-radius: 8px;
+            border-bottom-left-radius: 8px;
+        }
+        .fechar{
+            font-size: 1rem;
+            background-color: #f50e0e8f;
+            border-top-right-radius: 8px;
+            border-bottom-right-radius: 8px;
+            box-shadow: 0 5px 5px rgba(0, 0, 0, 0.1);
+            margin: 1rem auto;
+        }
+        .fechar:hover{
+            font-size: 1rem;
+            background-color: #a700008f;
+            border-top-right-radius: 8px;
+            border-bottom-right-radius: 8px;
+
+        }
+
+
+
+
+    </style>
